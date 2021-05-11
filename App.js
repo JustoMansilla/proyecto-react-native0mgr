67d0385db6636a764f8988f6856e072defbc7f9a
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-05-11 12:36:05 -0300

    Boton apretar y las alertas

--- App.js
+++ App.js
@@ -2,18 +2,33 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View,
+  Image,
   Button,
-  SafeAreaView
+  SafeAreaView,
+  Alert
 } from 'react-native';
 
 export default class App extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        contador: 0,
+        contador: 0,  
       }
     }
+
+
+    showAlert() {
+      Alert.alert(
+        'Alert title',
+        "Alert message",
+        [{ text: "Ask me later", onPress: () => console.log("Se presiono ask me later") },
+         { text: "OK", onPress: () => console.log("Se presiono OK") },
+         {text: "Cancel", onPress: () => console.log("Se presiono CANCEL")}]
+      )
+    }
+
+
+
 
     render(){
       return(
@@ -23,6 +38,11 @@ export default class App extends Component {
           <Button title="Sumar" onPress={() => {this.setState({contador: this.state.contador + 1})}}/>
           <Button title="Restar" onPress={() => {this.setState({contador: this.state.contador - 1})}}/>
           <Button title="Resetear" onPress={() => {this.setState({contador: this.state.contador = 0})}}/>
+
+          <Button title="APRETAR!" color="blue" onPress={this.showAlert.bind(this)}/>
+
+          <Image style={{width: 100, height: 100}} source={{uri: 'https://www.concienciaeco.com/wp-content/uploads/2015/08/Pantera-Negra-Vegetariana_thumb.jpg'}}/> 
+          
         </SafeAreaView>
 
         
