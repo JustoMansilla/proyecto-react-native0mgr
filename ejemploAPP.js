15e46d1da134c5522b5df4a10ef3e2260a75ae88
Lucas Grosz <lucasgrosz@MacBook-Pro-de-Lucas.local>
2021-05-28 12:12:35 -0300

    semiapi
    intentamos

---
+++ ejemploAPP.js
@@ -0,0 +1,58 @@
+// import React, { Component } from 'react';
+// import {
+//   Text,
+//   Image,
+//   Button,
+//   SafeAreaView,
+//   Alert,
+//   View
+// } from 'react-native';
+
+// export default class App extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         contador: 0,  
+//       }
+//     }
+
+
+//     showAlert() {
+//       Alert.alert(
+//         'Alert title',
+//         "Alert message",
+//         [{ text: "Ask me later", onPress: () => console.log("Se presiono ask me later") },
+//          { text: "OK", onPress: () => console.log("Se presiono OK") },
+//          {text: "Cancel", onPress: () => console.log("Se presiono CANCEL")}]
+//       )
+//     }
+
+
+
+
+//     render(){
+//       return(
+
+//         <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
+          
+//             <View style={{backgroundColor: 'red', flex: 1}}></View>
+//             <View style={{backgroundColor: 'blue', flex: 1}}></View>
+//             <View style={{backgroundColor: 'green', flex: 1}}></View>
+
+//           <Text>Contador: {this.state.contador}</Text>
+//           <Button title="Sumar" onPress={() => {this.setState({contador: this.state.contador + 1})}}/>
+//           <Button title="Restar" onPress={() => {this.setState({contador: this.state.contador - 1})}}/>
+//           <Button title="Resetear" onPress={() => {this.setState({contador: this.state.contador = 0})}}/>
+
+//           <Button title="APRETAR!" color="blue" onPress={this.showAlert.bind(this)}/>
+
+//           <Image style={{width: 100, height: 100}} source={{uri: 'https://www.concienciaeco.com/wp-content/uploads/2015/08/Pantera-Negra-Vegetariana_thumb.jpg'}}/> 
+          
+//         </SafeAreaView>
+
+        
+
+//       );
+//     }
+// };
+
