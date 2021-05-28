67f9295c35a4b46cd1b62a6c8f565e162c0e9336
Justo Enrique Mansilla <jmansilla@udesa.edu.ar>
2021-05-28 12:36:21 -0300

    Async comienzo

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import {
   View
 } from 'react-native';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 export default class App extends Component {
     constructor(props) {
       super(props);
@@ -23,16 +25,22 @@ export default class App extends Component {
     }
 
     async getApi () {
-    try {
-       let respuesta = await fetch(
-      "https://randomuser.me/api/"
-    );
-       
-    } catch (error) {
-   console.error(error);
+      try {
+        let respuesta = await fetch("https://randomuser.me/api/");
+      }catch (error) {
+        console.log(error);
       }
     }
 
+   
+
+    async removeItem(key) {
+      try {
+        await AsyncStorage.removeItem(key);  
+      } catch (error){
+        console.log(error);
+      }
+    }
     
     // const ArticlesFromApi = async () => {
     //   try {
@@ -52,7 +60,9 @@ export default class App extends Component {
     render(){
       return(
         <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
+
         <Text> hola </Text>
+        
         </SafeAreaView>
         
 
